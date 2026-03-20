7a11f841f3bccb49694cee95189f3fa916426f81
Funke Sowole <funkegoodvibe@gmail.com>
2026-03-20 09:24:28 +0000

    pytest test

--- pytest::intro_to_testing.py
+++ pytest::intro_to_testing.py
@@ -377,3 +377,78 @@ class TestSuite(unittest.TestCase):
     def test_is_prime(self):
         # Check that 6 is not prime
         self.assertFalse(is_prime(6))
+
+
+
+#Run unittest from CLI
+"""
+python3 -m unittest test_sqneg.py 
+
+
+Keyword arg: 
+python3 -m unittest -k "SomeStringOrPattern" test_sqneg.py 
+
+Fail fast flag: 
+python3 -m unittest -f test_sqneg.py 
+
+Catch Flag: 
+python3 -m unittest -c test_sqneg.py 
+
+Verbose Flag:
+python3 -m unittest -v test_sqneg.py 
+"""
+
+
+import unittest
+import unittest
+
+class TestWord(unittest.TestCase):
+    # Fixture setup method
+    def setUp(self):
+        # Initialize the word banana here
+        self.word = 'banana'
+
+    # Test method
+    def test_the_word(self):
+        # Add the tests here
+        self.assertNotIn('B', self.word)
+        self.assertNotIn('y', self.word)
+        self.assertIn('b', self.word)
+    
+    # Fixture teardown method
+    def tearDown(self):
+        # Delete the word variable here
+        del self.word
+
+#python3 -m unittest palindrome_check.py
+
+
+
+
+#Activity X
+import unittest
+
+def check_palindrome(string):
+    reversed_string = string[::-1]
+    return string == reversed_string
+
+def create_data():
+    return ['level', 'step', 'peep', 'toot']
+
+class TestPalindrome(unittest.TestCase):
+    def setUp(self):
+        # Initialize data here
+        self.data = create_data()
+    
+    def test_func(self):
+        expected_result = [True, False, True, True]
+        data_checked = list(map(check_palindrome, self.data))
+        # Verify the checked data here
+        self.assertEqual(data_checked, expected_result)
+
+    def tearDown(self):
+        # Clear the data here
+        self.data.clear()
+
+#python3 -m unittest palindrome_check.py
+
